c50a9204e6baa7f8b1b1d399c7b4a05ac1d85c02
LepsyMikolaj3301 <miki2004200@wp.pl>
2024-12-01 16:40:58 +0100

    Wyjebanie wszystkiego i zapis głównych założeń symulacji.

--- src/main.py
+++ src/main.py
@@ -1,27 +1,49 @@
 """
 Main program
 
+Przebieg symulacji (szczegółowy):
+    Pętla przedstawia przebieg czasowy, w którym będzie
+    zasymulowany odstęp czasowy (1 minuta)
+
+    Przebieg klienta w symulacji:
+        Wygenerowanie klienta (w zależności od odst. czasowego)
+        
+        klient ze zrobionymi zakupami zostaje przydzielony do kolejki 
+            (kolejek gdy są obsługowe kasy)
+        
+        * Kolejka *
+            Gdy kasa pusta -> przydziel do kasy klienta
+
+        Klient przy kasie:
+            - kasowanie i checkout
+        
+        Deportacja klienta xd
+    
+            
+
+
 Importuje:
     - stats_sym.py
+    - util_klasy.py
 
 
 Zawiera:
     wywoławcza main
     
-    klasa Sklep
-    
-    funkcja run
 
 Notes:
-    - simpy.Resource() używany do tego, gdy jest coś ograniczonego
-    przykład u nas: kasy ( tylko jakie? )
-    
-przykładowy projekt
-https://realpython.com/simpy-simulating-with-python/
+     * architektura potrzebna do przypisywania wygen. klientów do kolejek
+     dla różnych klas *
 
+DONE:
+    gówno
+
+
+TODO:
+    - WIZUALIZACJA I UI W CMDku ( pretty print ) !!!
+    - glowna petla
+    - 
 """
-
-import simpy
 
 import json
 
@@ -31,43 +53,3 @@ def odczyt_pliku_ster() -> dict:
         parametry = json.load(file)
         return parametry
 
-# TO DO:
-# napisać klase Klient, która będzie obszerna z róznymi parametrami
-# klasa klient będzie generowana później w funkcji przeprowadzającej symulacje
-# czyli funkcja zrob_zakupy()
-
-
-class Sklep:
-    def __init__(self, env, plik_ster):
-        # parametry
-        parametry = odczyt_pliku_ster()
-
-        self.env = env
-        self.ilosc_kas = tuple(parametry["liczba_kas_samoobslugowych"],
-                               parametry['liczba_kas_zwyklych'])
-        
-        if self.ilosc_kas[0]:
-            self.kasa_sam = simpy.Resource(env, self.ilosc_kas[0])
-        else:
-            self.kasa_sam = None
-
-        if self.ilosc_kas[1]:
-            self.kasa_zw = simpy.Resource(env, self.ilosc_kas[1])
-        else:
-            self.kasa_zw = None
-
-
-
-        pass
-
-    # to do:
-    # napisac funkcje skasuj_zakupy()
-
-
-# zrob_zakupy() będzie funkcją która tyczy się pojedyńczego klienta
-# ich czas zakupów będzie zależeć od parametrów z kartki i ich rozkładów
-# ale starajmy się, by ich rozkłady nie były z dużym odchyleniem std. by symulacja była wiarygodna
-
-# funkcja statystyk
-
-# funkcja main ( do ogarnięcia muzgowego )
---
+++ src/util_klasy.py
@@ -0,0 +1,11 @@
+"""
+    util_klasy.py czyli program zawierający wszystkie potrzebne obiekty
+    do głównego programu.
+
+    Zawartosc:
+        - class Klient
+        - class Kasa
+    
+    Dodatkowo:
+        - 
+"""
