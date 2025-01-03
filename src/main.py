d178392cc2c39d77e06f3b590746d1b0f2119db1
LepsyMikolaj3301 <miki2004200@wp.pl>
2025-01-03 17:18:00 +0100

    male zmiany w main, boże trzeba ruszyć to cuś

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ Notes:
      * architektura potrzebna do przypisywania wygen. klientów do kolejek
      dla różnych klas *
 
-    CZY WPROWADZENIE TICK = 1 sekunda MA SENS?
+    
 
 DONE:
     gówno
@@ -56,20 +56,41 @@ def odczyt_pliku_ster() -> dict:
         parametry = json.load(file)
         return parametry
 
-def losuj_czas_mdzy_klientami(ranga_dnia: int):
+def losuj_czas_nowy_klient(ranga_dnia: int):
     """ TA FUNKCJA LOSUJE CZAS MIĘDZY 'PRZYCHODZENIEM' NOWYCH KLIENTÓW DO SKLEPU
 
     Args:
         rodzaj_dnia (int): RANGA dnia ( czy duży / średni / mały RUCH w sklepie)
     """
-    pass
+    match ranga_dnia:
+        # dzień customowy
+        case 0: 
+            pass
+        # dzień roboczy - mały ruch ( środa w lidlu )
+        case 1:
+            pass
+        # dzień zwykły - zwyczajny ruch ( sobota w lidlu )
+        case 2:
+            pass
+        # dzień ruchliwy - duży ruch ( BLACK FRIDAY w lidlu )
+        case 3:
+            pass
+        
 
-class SymulacjaKasy:
+class Symulacja:
 
     
 
     def __init__(self):
         pass
+
+    
+    def symulacja():
+        """
+        GLOWNA PETLA SYMULACJI
+        """
+
+
 
 
 
